1b163dffe460c32618d4612a2e569101911c1bec
IonelaCristinaNistor <nistorc71@gmail.com>
2023-12-02 23:51:49 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,21 +3,9 @@ import tailwind from "@astrojs/tailwind";
 
 import mdx from "@astrojs/mdx";
 
-const SERVER_PORT = 3000;
-const LOCALHOST_URL= 'http://localhost:${SERVER_PORT}';
-const LIVE_URL = "https://IonelaCristinaNistor.github.io";
-
-const SCRIPT = process.env.npm_lifecycle_script || "";
-const isBuild = SCRIPT.includes("astro build");
-let BASE_URL = LOCALHOST_URL;
-if(isBuild) {
-  BASE_URL = LIVE_URL;
-}
-
 // https://astro.build/config
 export default defineConfig({
-  server: {port: SERVER_PORT},
-  site: BASE_URL,
-  integrations: [sitemap(), tailwind(), mdx()],
+  site: "https://IonelaCristinaNistor.github.io",
+  integrations: [tailwind(), mdx()],
   output: "server"
 });
